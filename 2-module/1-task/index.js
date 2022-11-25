bff7246f03b5af6474627a3cb60a2ca97e45e573
tyshenko61 <34244584+tyshenko61@users.noreply.github.com>
2022-11-25 08:07:20 +0500

    2-module\1-task исправлено на сравнение с учетом типа, хотя тип был проверен отдельно

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,7 +1,7 @@
 function sumSalary(salaries) {
   let sum = 0;
   for (let key in salaries) {
-    if (typeof(salaries[key]) === 'number' && !isNaN(salaries[key]) && salaries[key] != Infinity && salaries[key] != -Infinity) {
+    if (typeof(salaries[key]) === 'number' && !isNaN(salaries[key]) && salaries[key] !== Infinity && salaries[key] !== -Infinity) {
       sum += salaries[key];
     }
   }
