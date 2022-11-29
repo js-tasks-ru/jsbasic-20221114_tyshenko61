02416b90595f51f0542ec7a42453a937f2f1a464
tyshenko61 <34244584+tyshenko61@users.noreply.github.com>
2022-11-29 15:43:27 +0500

    3-module\5-task исправлено условиее фильтра

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,5 +1,5 @@
 function getMinMax(str) {
-  let arr = str.split(' ').map(item => Number(item)).filter(item => Number.isFinite(item));
+  let arr = str.split(' ').map(item => Number(item)).filter(item => Boolean(item ));
   arr.sort((a,b) => a - b);
   return { min: arr[0],
            max : arr[arr.length-1]};
