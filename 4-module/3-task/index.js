2de9eefe369d1f844d56e28a8157b3750f610257
tyshenko61 <34244584+tyshenko61@users.noreply.github.com>
2022-12-03 17:59:03 +0500

    4-module\3-task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,20 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 0; i < table.rows.length; i++) {
+    if (parseInt(table.rows[i].cells[1].innerText) < 18 ) {
+       table.rows[i].style.textDecoration = "line-through";
+    }
+    if (table.rows[i].cells[2].innerText === "m") {
+      table.rows[i].classList.add('male');
+    } else if (table.rows[i].cells[2].innerText === "f") {
+      table.rows[i].classList.add('female');
+    }
+    if (table.rows[i].cells[3].dataset.available === "true") {
+      table.rows[i].classList.add('available');
+    } else if (table.rows[i].cells[3].dataset.available === "false") {
+      table.rows[i].classList.add('unavailable');
+    }
+    else {
+      table.rows[i].setAttribute("hidden", "true");
+    }
+  }
 }
